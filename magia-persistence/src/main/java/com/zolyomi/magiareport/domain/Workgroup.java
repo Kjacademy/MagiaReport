85bc1bcde9bf9d49f60cd63729720eafebcb0f69
Janos Kiss <Janos_Kiss@epam.com>
2019-12-07 15:24:09 +0100

    MagiaReport-pesistence Initial state.

---
+++ magia-persistence/src/main/java/com/zolyomi/magiareport/domain/Workgroup.java
@@ -0,0 +1,13 @@
+package com.zolyomi.magiareport.domain;
+
+@Entity
+public class Workgroup {
+    private Long id;
+    private String name;
+    private String sourceCode;
+    private String parent;
+    private String status;
+    private String escalation;
+    private String owner;
+    private String group;
+}
